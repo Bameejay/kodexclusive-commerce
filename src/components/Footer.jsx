6c24da6a34ac09e92b0a33c7bbca6dbd75af8362
bameejay <johnsonkayode01@gmail.com>
2024-07-07 23:51:24 +0100

    footer responsiveness fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,55 @@
 const Footer = () => {
-    return ( 
-        <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800">
+    return (
+		<div className="bg-black">
+        <footer className=" container mx-auto px-4 divide-y text-white dark:bg-gray-100 dark:text-gray-800">
 	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
-		<div className="lg:w-1/3">
-			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
-				<div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-50">
-						<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-					</svg>
-				</div>
-				<span className="self-center text-2xl font-semibold">Brand name</span>
-			</a>
+		<div className="space-y-6 lg:w-1/3">
+			<span className="self-center text-lg font-semibold">Exclusive</span>
+			<h3 className="text-sm">Subscribe</h3>
+			<p className="font-normal text-sm">Get 10% off your first order</p>
+			<input type="text" placeholder="Enter your email" className="border bg-black text-xs p-2" />
 		</div>
-		<div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
-			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-900">Product</h3>
-				<ul className="space-y-1">
+		{/* <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4"> */}
+			<div className="space-y-6">
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Support</h3>
+				<p>111 Bijoy sarani, Dhaka,<br /> DH 1515, Bangladesh.</p>
+				<p>exclusive@gmail.com</p>
+				<p>+88015-88888-9999</p>
+			</div>
+			<div className="space-y-6">
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Account</h3>
+				<ul className="space-y-4">
 					<li>
-						<a rel="noopener noreferrer" href="#">Features</a>
+						<a rel="noopener noreferrer" href="#">My Account</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Integrations</a>
+						<a rel="noopener noreferrer" href="#">Login/Register</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Pricing</a>
+						<a rel="noopener noreferrer" href="#">Cart</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Wishlist</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Shop</a>
+					</li>
+				</ul>
+			</div>
+			<div className="space-y-6">
+				<h3 className="uppercase dark:text-gray-900">Quick Link</h3>
+				<ul className="space-y-4">
+					<li>
+						<a rel="noopener noreferrer" href="#">Privacy Policy</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Terms Of Use</a>
 					</li>
 					<li>
 						<a rel="noopener noreferrer" href="#">FAQ</a>
 					</li>
-				</ul>
-			</div>
-			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-900">Company</h3>
-				<ul className="space-y-1">
 					<li>
-						<a rel="noopener noreferrer" href="#">Privacy</a>
-					</li>
-					<li>
-						<a rel="noopener noreferrer" href="#">Terms of Service</a>
-					</li>
-				</ul>
-			</div>
-			<div className="space-y-3">
-				<h3 className="uppercase dark:text-gray-900">Developers</h3>
-				<ul className="space-y-1">
-					<li>
-						<a rel="noopener noreferrer" href="#">Public API</a>
-					</li>
-					<li>
-						<a rel="noopener noreferrer" href="#">Documentation</a>
-					</li>
-					<li>
-						<a rel="noopener noreferrer" href="#">Guides</a>
+						<a rel="noopener noreferrer" href="#">Contact</a>
 					</li>
 				</ul>
 			</div>
@@ -75,10 +73,11 @@ const Footer = () => {
 					</a>
 				</div>
 			</div>
-		</div>
+		{/* </div> */}
 	</div>
-	<div className="py-6 text-sm text-center dark:text-gray-600">© 1968 Company Co. All rights reserved.</div>
+	<div className="py-6 text-sm text-center dark:text-gray-600">© 1968 Jaycodes build. All rights reserved.</div>
 </footer>
+</div>
      );
 }
  
