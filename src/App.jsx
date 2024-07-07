8c5c5aa672f8b8b3c9720496fd27aea1a094d4d6
bameejay <johnsonkayode01@gmail.com>
2024-07-07 06:47:21 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <p className='bg-yellow-600 text-center text-white text-xs p-2'>I will like to continue using tailwindcss to style, because it is the best</p>
+    </div>
+  )
+}
+
+export default App
