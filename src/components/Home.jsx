3d80e7b55e2784c7ed6be113e71e557e705cea07
bameejay <johnsonkayode01@gmail.com>
2024-07-09 11:51:27 +0100

    added flash sales

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,54 +12,53 @@ import { FaGreaterThan } from "react-icons/fa";
 
 const Home = () => {
 
-    // const calculateTimeLeft = () => {
-    //     const difference = +new Date(targetDate) - +new Date();
-    //     let timeLeft = {};
+    const [timeLeft, setTimeLeft] = useState({
+        days: 30,
+        hours: 23,
+        minutes: 19,
+        seconds: 56,
+      });
     
-    //     if (difference > 0) {
-    //       timeLeft = {
-    //         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    //         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    //         minutes: Math.floor((difference / 1000 / 60) % 60),
-    //         seconds: Math.floor((difference / 1000) % 60),
-    //       };
-    //     }
+      useEffect(() => {
+        const countdownDate = new Date().getTime() + 30 * 24 * 60 * 60 * 1000; // 3 days from now
+        const interval = setInterval(() => {
+          const now = new Date().getTime();
+          const distance = countdownDate - now;
     
-    //     return timeLeft;
-    //   };
+          const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    //   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+          setTimeLeft({
+            days: String(days).padStart(2, "0"),
+            hours: String(hours).padStart(2, "0"),
+            minutes: String(minutes).padStart(2, "0"),
+            seconds: String(seconds).padStart(2, "0"),
+          });
     
-    //   useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //       setTimeLeft(calculateTimeLeft());
-    //     }, 1000);
+          if (distance < 0) {
+            clearInterval(interval);
+          }
+        }, 1000);
     
-    //     return () => clearTimeout(timer);
-    //   }, [timeLeft]);
-    
-    //   const timerComponents = [];
-    
-    //   Object.keys(timeLeft).forEach((interval) => {
-    //     timerComponents.push(
-    //       <span key={interval} className="mx-1">
-    //         {timeLeft[interval]} {interval}
-    //       </span>
-    //     );
-    //   });
+        return () => clearInterval(interval);
+      }, []);
 
 
 
     return ( 
-        <div className='container mx-auto flex divide-x mb-20'>
+        <div className='container mx-auto'>
+            
+            <div className='flex mb-20'>
             {/* Left Section */}
-            <div className='w-1/2 flex' >
-                <ul className='space-y-3 text-sm'>
+            <div className='w-1/4 flex mt-4 mb-20' >
+                <ul className='space-y-5 text-sm'>
                     <li>
-                        <Link to="" className='flex items-center gap-20'>Woman's Fashion<FaGreaterThan/> </Link>
+                        <Link to="" className='flex items-center gap-[52px]'>Woman's Fashion<FaGreaterThan/> </Link>
                     </li>
                     <li>
-                        <Link to="" className='flex items-center gap-[88px]'>Men's Fashion <FaGreaterThan/></Link>
+                        <Link to="" className='flex items-center gap-20'>Men's Fashion <FaGreaterThan/></Link>
                     </li>
                     <li>
                         <Link to="">Electronics</Link>
@@ -83,17 +82,59 @@ const Home = () => {
                         <Link to="">Health & Beauty</Link>
                     </li>
                 </ul>
-                <div className="w-0.5 h-72 bg-gray-400 ml-2 -mt-4"></div>
+                <div className="w-0.5 bg-gray-400 ml-2 -mt-8"></div>
+            </div>
 
+                {/* Right Section */}
+                <div className='mt-4 hero-image'>
+                    <img src={heroImage} alt="hero_image" className='object-contain bg-red-600 w-[100%]' />
+                </div>
             </div>
 
 
-            {/* Right Section */}
-            <div className=''>
-                <img src={heroImage} alt="hero_image" className='object-contain bg-red-600' />
-            </div>
+            {/* Flash Sale */}
+            <div className="flex flex-col">
+                <div className="flex items-center">
+                    <div className="bg-primaryColor w-4 h-10 mr-4 rounded-md"></div>
+                    <span className="text-red-500 font-semibold text-xl">Today's</span>
+                </div>
+                <div className='flex space-x-20 mt-4 mb-10 items-end'>
+                    <div className="text-4xl font-bold">Flash Sales</div>
+                    <div className="flex space-x-4">
+                        <div className="text-center">
+                            <div className="text-lg text-gray-500 font-bold">Days</div>
+                            <div className="countdown-number text-4xl font-bold">
+                            {timeLeft.days}
+                            </div>
+                        </div>
+                    <div className="text-red-500 text-4xl mt-6">:</div>
+                        <div className="text-center">
+                            <div className="text-lg text-gray-500 font-bold">Hours</div>
+                            <div className="countdown-number text-4xl font-bold">
+                            {timeLeft.hours}
+                            </div>
+                        </div>
+                    <div className="text-red-500 text-4xl mt-6">:</div>
+                        <div className="text-center">
+                            <div className="text-lg text-gray-500 font-bold">Minutes</div>
+                            <div className="countdown-number text-4xl font-bold">
+                            {timeLeft.minutes}
+                            </div>
+                        </div>
+                    <div className="text-red-500 text-4xl mt-6">:</div>
+                        <div className="text-center">
+                            <div className="text-lg text-gray-500 font-bold">Seconds</div>
+                            <div className="countdown-number text-4xl font-bold">
+                            {timeLeft.seconds}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+            </div>
         </div>
+
+
      );
 }
  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import { Outlet, Link } from "react-router-dom";
-// import { GrCart } from "react-icons/gr";
+import { IoCartOutline } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoIosHeartEmpty } from "react-icons/io";
+
 
 
 
@@ -24,13 +27,37 @@ const Navbar = () => {
                 {/* Logo */}
                     <div className="pt-2 text-secondaryColor text-xl font-bold cursor-pointer">Exclusive</div>
                 {/* Menu-Items */}
-                <div className="hidden md:flex space-x-6">
+                <div className="hidden md:flex space-x-10">
                     <Link to="/" className="text-sm text-secondaryColor font-medium">Home</Link>
                     <Link to="/contact" className="text-sm text-secondaryColor font-medium">Contact</Link>
                     <Link to="/" className="text-sm text-secondaryColor font-medium">About</Link>
                     <Link to="/" className="text-sm text-secondaryColor font-medium">Sign Up</Link>
                 </div>
+
+                {/* Search and Cart Section*/}
+            <div className="flex gap-4 items-center">
+                <div className='flex items-center p-2 border rounded-lg bg-gray-200'>
+                    <input type="text" placeholder="What are you looking for?" className=" border border-none focus:outline-none bg-gray-200 text-sm" />
+                    <IoSearchOutline className="text-gray-500 ml-2" />
+                </div>
+                {/* Wish List Icon */}
+                <Link to='/'>
+                    <IoIosHeartEmpty className="w-6 h-6" />
+                </Link>
+                {/* Cart Icon */}
+                <Link to='/'>
+                    <IoCartOutline className="w-6 h-6"/>
+                </Link>
             </div>
+                
+                
+                
+                
+                
+                
+            </div>
+
+            
         </nav>
         <div className="w-full h-0.5 bg-gray-400 mb-4"></div>
 
